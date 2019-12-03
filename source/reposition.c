708ec17cba9371ec0a1508f6401e0a3f58084ab1
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2019-12-03 10:53:24 +0000

    Remove unwanted function and includes

--- source/reposition.c
+++ source/reposition.c
@@ -13,17 +13,13 @@
  *
  ***********************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
 #include "atomic.h"
 #include "python.h"
 
 
 /**********************************************************/
 /**
- * @brief      p) attempts to assure that a photon is not scattered
+ * @brief attempts to assure that a photon is not scattered
  * 	a second time inappropriately by the same transition
  *
  * @param [in,out] PhotPtr  p   A photons
@@ -57,52 +53,4 @@ reposition (PhotPtr p)
   move_phot (p, wmain[p->grid].dfudge);
 
   return (0);
-}
-
-/* ************************************************************************* */
-/**
- * @brief           Reposition a photon which was lost due to dfudge pushing
- *                  the photon into the disk
- *
- * @param[in,out]   PhotPtr   p     The photon to be repositioned
- *
- * @return          void
- *
- * @details
- *
- * For resonant scatters, this function will push the photon a distance away
- * from the location where it previously interacted to assure that the photon
- * does not interact with the same resonance twice.
- *
- * Created in response to issue #584 on the GitHub repository. The purpose of
- * this function is to calculate the distance to the surface of the accretion
- * disc and to make it some distance towards the disc instead of dfudge, which
- * previously pushed the photon through the disc plane accidentally.
- *
- * ************************************************************************** */
-
-void
-reposition_lost_disk_photon (PhotPtr p)
-{
-  double smax;
-
-  if (p->nres < 0)
-    return;                     /* Do nothing for non-resonant scatters */
-
-  if ((p->grid = where_in_grid (wmain[p->grid].ndom, p->x)) < 0)
-  {
-    Error ("%s:%s(%i): Photon %i not in grid\n", __FILE__, __func__, __LINE__, p->np);
-    return;
-  }
-
-  smax = -p->x[2] / p->lmn[2];
-
-  if (smax < 0)
-  {
-    Error ("%s:%s(%i): Photon %i unable to be repositioned correctly toward the disk\n", __FILE__, __func__, __LINE__, p->np);
-    return;
-  }
-
-  smax *= SMAX_FRAC;
-  move_phot (p, smax);
 }
